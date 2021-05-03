893be0aafed4564d0422219de3cb1f06b8778431
joeljk <joel.joyston@gmail.com>
2021-05-03 19:11:47 +0530

    library app for review

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookaddComponent } from './inventory/bookadd/bookadd.component';
+import { BookdeleteComponent } from './inventory/bookdelete/bookdelete.component';
+import { BookeditComponent } from './inventory/bookedit/bookedit.component';
+import { LoginComponent } from './login/login.component';
+import { AuthguardService } from './shared/authguard.service';
+import { BookreturnComponent } from './transaction/bookreturn/bookreturn.component';
+import { SearchbookComponent } from './transaction/searchbook/searchbook.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/login', pathMatch:'full'},
+  {path:'login', component:LoginComponent},
+  {path:'addBook', component:BookaddComponent},
+  {path:'editBook', component:BookeditComponent},
+  {path:'deleteBook', component:BookdeleteComponent},
+  {path:'borrow',component:SearchbookComponent},
+  {path:'return',component:BookreturnComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { BookaddComponent } from './inventory/bookadd/bookadd.component';
+import { LoginComponent } from './login/login.component';
+import { SearchbookComponent } from './transaction/searchbook/searchbook.component';
+import { BookeditComponent } from './inventory/bookedit/bookedit.component';
+import { BookreturnComponent } from './transaction/bookreturn/bookreturn.component';
+import { BookdeleteComponent } from './inventory/bookdelete/bookdelete.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InventoryComponent,
+    TransactionComponent,
+    BookaddComponent,
+    SearchbookComponent,
+    LoginComponent,
+    BookeditComponent,
+    BookreturnComponent,
+    BookdeleteComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
